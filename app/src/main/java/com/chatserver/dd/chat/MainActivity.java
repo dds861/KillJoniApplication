b4990ead2c3c9fb5c40dbeb2dc326db274034e34
dds861 <dds861@gmail.com>
2017-09-09 03:09:07 +0200

    new code

--- app/src/main/java/com/chatserver/dd/chat/MainActivity.java
+++ app/src/main/java/com/chatserver/dd/chat/MainActivity.java
@@ -3,14 +3,15 @@ package com.chatserver.dd.chat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.chatserver.dd.chat.Menu1.ActivityMenu1;
 import com.chatserver.dd.chat.Menu2.ActivityMenu2;
-
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 import static com.chatserver.dd.chat.R.layout.activity_main;
 
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     private AdView mAdView;
+
+    private InterstitialAd mInterstitialAd;
 
     private Button mBtn1;
     private Button mBtn2;
@@ -33,6 +36,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3941279894802307/3018755401");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
     }
 
     private void initView() {
@@ -46,10 +53,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn1:
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                } else {
+                    Log.d("TAG", "The interstitial wasn't loaded yet.");
+                }
                 Intent intent1 = new Intent(this, ActivityMenu1.class);
                 startActivity(intent1);
                 break;
             case R.id.btn2:
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                } else {
+                    Log.d("TAG", "The interstitial wasn't loaded yet.");
+                }
                 Intent intent2 = new Intent(this, ActivityMenu2.class);
                 startActivity(intent2);
                 break;
